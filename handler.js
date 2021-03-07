fa3f16616924e9ffededf467c433c80871e5a7e8
UshiJima (Kazuki, 67-inch, Late 1995) <info@kfs214.net>
2021-03-07 17:14:24 +0900

    .gitignore is now working

--- handler.js
+++ handler.js
@@ -1,17 +1,28 @@
 "use strict";
 
+const josephJoestar = (input_text) => {
+  const pattern = /「.+」/;
+  const quoted_text = input_text.match(pattern);
+  if (quoted_text) {
+    return `your message is \"${input_text}\"`;
+  } else {
+    return `「quoted_text」……はっ！`;
+  }
+};
+
 module.exports.echo = async (event) => {
-  console.log(event);
+  const message = event.body.message;
+  const replying_message = josephJoestar(message);
+
   return {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
     },
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: replying_message,
         input: event,
       },
       null,
